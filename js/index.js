8fbf5e3a2e958ffdb8fd8b62330aa2e3e60373fc
ann0101 <anna.protsyk22@gmail.com>
2019-10-20 22:19:31 +0000

    Almost finished working with СSS

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-function myFunctionMenu() {
-    var x = document.getElementById("menu");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
+function generetedNumbers(){
+    return;
 }
-

8f347b3558e0045d6bc0270ac252d1af6c788cd5
ann0101 <anna.protsyk22@gmail.com>
2019-10-21 13:44:41 +0000

    add script for generated numbers, change css

---
+++ js/genereted-number.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.generated-number').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.cell(now));
+            }
+        });
+    });
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
-function generetedNumbers(){
-    return;
-}
+/*const newsList = new NewsList(
+  'news.json',
+  $('.news-list')
+);
+
+class GeneretedNumbers {
+  constructor(productsUrl, renderContainer, cart) {
+    this.cart = cart;
+    fetch(productsUrl)
+      .then(result => result.json())
+      .then(products => {
+        products.sort( (a, b) => a.price - b.price );
+        this.products = products;
+        this.renderProducts(renderContainer, products);
+        this.addEventListeners();
+        document.querySelector('.filter').addEventListener('keydown',
+            () => this.renderProducts(renderContainer, products));
+      });
+  }
+  getProductById(id) {
+    return this.products.find(el => el.id === id);
+  }
+function generetedNumbers($'.generated-number'){
+  return Math.floor(Math.random()*40);
+}*/

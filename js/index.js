c980d223f7f261b7c8900b3425bc26caba76943d
ann0101 <anna.protsyk22@gmail.com>
2019-10-21 15:12:02 +0000

    change search

--- js/index.js
+++ js/index.js
@@ -1,25 +1,13 @@
-/*const newsList = new NewsList(
-  'news.json',
-  $('.news-list')
-);
-
-class GeneretedNumbers {
-  constructor(productsUrl, renderContainer, cart) {
-    this.cart = cart;
-    fetch(productsUrl)
-      .then(result => result.json())
-      .then(products => {
-        products.sort( (a, b) => a.price - b.price );
-        this.products = products;
-        this.renderProducts(renderContainer, products);
-        this.addEventListeners();
-        document.querySelector('.filter').addEventListener('keydown',
-            () => this.renderProducts(renderContainer, products));
-      });
-  }
-  getProductById(id) {
-    return this.products.find(el => el.id === id);
-  }
-function generetedNumbers($'.generated-number'){
-  return Math.floor(Math.random()*40);
-}*/
+$(document).ready(function(){
+    $('.generated-number').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
